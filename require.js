3cb8fd4ea538920c925467e2d2e26a58561c9895
ksw06086 <k-sunwo@naver.com>
2023-04-26 17:59:42 +0900

    node.js: 3강ECMAScript모듈, exports, this, 노드내장객체

---
+++ require.js
@@ -0,0 +1,4 @@
+require(`./var`);
+console.log(require);
+
+require.cache[`C:\\Dev50\\Node.js_Programming\\require.js\\var.js`].exports
---
+++ this.js
@@ -0,0 +1,5 @@
+console.log(this);
+
+function a() {
+    console.log(this === global);
+}
