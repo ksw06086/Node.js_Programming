7284051bcd911eee3c605c82c742609e89d1baba
ksw06086 <k-sunwo@naver.com>
2023-04-27 14:06:05 +0900

    node.js: 3강URL,DNS, crypto, util

---
+++ url.js
@@ -0,0 +1,3 @@
+const myURL = new URL('https://www.inflearn.com/course/lecture?courseSlug=%EB%85%B8%EB%93%9C-js-%EA%B5%90%EA%B3%BC%EC%84%9C&unitId=143559')
+console.log('new URL():', myURL)
+console.log('url.format():', url.format(myURL));
