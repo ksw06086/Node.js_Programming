455dd2c883ed34c0299b6d3f54762b3ea1779d4c
ksw06086 <k-sunwo@naver.com>
2023-07-18 13:04:35 +0900

    node.js - 미들웨어 사용하기

--- 501. express 서버 사용/app.js
+++ 501. express 서버 사용/app.js
@@ -8,6 +8,13 @@ const app = express();
 // (위험 - 앞으로 계속 process.env.PORT는 해당 포트번호가 됨, 다른 프로그램 실행 시 문제 발생 가능성 큼)
 app.set('port', process.env.PORT || 3000); // 서버에 포트라는 속성의 값을 3000으로 넣음
 
+// 미들웨어 사용
+// 에러처리 미들웨어시 매개변수가 4개여야하는 조건이 충족되어야함
+app.use((req, res, next) => {
+    console.log("모든 요청에 실행하고 싶어요.");
+    next();
+})
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
@@ -18,6 +25,11 @@ app.post('/', (req, res) => {
 
 app.get('/about', (req, res) => {
     res.send('hello about');
+});
+
+// :변수 - 라우트 매개변수(= 와일드 카드), req.params.변수명 으로 사용
+app.get('/category/:name', (req, res) => {
+    res.send(`hello ${req.params.name}`);
 });
 
 // 여기서 가져와서 사용함(3000 포트를 의미)
