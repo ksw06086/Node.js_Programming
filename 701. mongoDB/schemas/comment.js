fff88c41f34af7ba3f67960d0f8ee87933d3562b
ksw06086 <k-sunwo@naver.com>
2023-07-26 21:49:19 +0900

    node.js - mongoose(몽구스) 스키마 사용하기

--- 701. mongoDB/schemas/comment.js
+++ 701. mongoDB/schemas/comment.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 
+// ObjectId 꺼내주지 않으면 mongoose.Schema.Types.ObjectId로 작성해주어야 함
 const { Schema } = mongoose;
 const { Types: { ObjectId } } = Schema;
 const commentSchema = new Schema({
@@ -17,5 +18,10 @@ const commentSchema = new Schema({
     default: Date.now,
   },
 });
+// ref: 'User'을 안쓰는 사람도 많음 NoSQL(특성이 중복 감수함)이기 때문에
+// 그래서 insert 할 때
+// commenter: {
+//   name: 'zero', age: 24, ... 이렇게 넣어주기도 함 -> 데이터 중복이 있을 수 있음, 하지만 접근이 쉬움
+// }
 
 module.exports = mongoose.model('Comment', commentSchema);
